7ab8972d11a59e184e4f7b9cd6bed81238287306
BintangP28 <77977048+BintangP28@users.noreply.github.com>
2021-02-03 16:08:13 +0700

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -1,17 +1,6 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
-Syarat dan Ketentuan Bot *KAMI*
-1. Teks dan nama pengguna WhatsApp anda akan kami simpan di dalam server selama bot aktif
-2. Data akan di hapus ketika bot Offline
-3. Kami tidak menyimpan gambar, video, file, audio, dan dokumen yang anda kirim
-4. Kami tidak akan pernah meminta anda untuk memberikan informasi pribadi
-5. Jika menemukan Bug/Error silahkan langsung lapor ke Owner bot
-6. Juga kalau kalian ingin buat TUTORIAL BOT Langsung Lewat
-     ➥ YouTube: https://www.youtube.com/c/DrawlNag
-6. Apapun yang anda perintah pada bot ini, KAMI TIDAK AKAN BERTANGGUNG JAWAB!
-
-Thanks !
-2021©Drawl Nag
+:V
 `.trim(), m)
 }
 handler.help = ['rules']
